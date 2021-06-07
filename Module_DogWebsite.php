c3ae474fa5cd4e069fa747dcb8bcd90cd23d193d
gizmore <gizmore@wechall.net>
2021-06-07 09:37:17 +0200

    6.10.4-r1160

--- Module_DogWebsite.php
+++ Module_DogWebsite.php
@@ -8,13 +8,13 @@ final class Module_DogWebsite extends GDO_Module
     public $module_priority = 100;
     public function isSiteModule() { return true; }
     public function getTheme() { return 'dog'; }
-    public function gdoDependencies() {
+    public function getDependencies() {
         return [
             'Classic', 'Dog', 'DogAuth', 'Login', 'Register', 'Admin',
             'DogIRC', 'DogTick', 'DogShadowdogs', 'DogIRCAutologin',
             'DogIRCSpider', 'DogGreetings',
             'News', 'PM', 'Quotes', 'Shoutbox', 'Forum', 'Links', 'Download',
-            'Math', 'Contact',
+            'Math', 'Contact', 'Todo',
         ];
     }
     
